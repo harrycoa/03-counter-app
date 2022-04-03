308341aebe84f5e2bbbc8b8c1f97e6c9bdbfe5ee
Harry Coa <rodol_2000@hotmail.com>
2022-04-03 18:01:31 -0500

    feat upgrade

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+import React from "react";
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import { ReactDOM } from 'react-dom';
+
+
+const saludo = <h1>Hola Mundo</h1>;
+const divRoot = document.querySelector("#root");
+
+console.log(divRoot);
+
 
 const root = createRoot(document.getElementById("root"));
+root.render(saludo);
+
+
+/*
+const root = createRoot(document.getElementById("root"));
 root.render(<App />);
-
-
-reportWebVitals();
+*/
